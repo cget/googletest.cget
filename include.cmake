8f9fb1f90c3a88ea77ca5847e3d139d50274afc3
Justin Berger <jberger@melco.com>
2016-10-18 13:23:08 -0600

    Added logic for shared crt

--- include.cmake
+++ include.cmake
@@ -2,5 +2,14 @@ if(NOT VERSION)
        set(VERSION release-1.8.0)
 endif()
 
-CGET_HAS_DEPENDENCY(googletest GITHUB google/googletest VERSION "${VERSION}" FINDNAME GTest)
+CGET_HAS_DEPENDENCY(ucm REGISTRY)
+
+ucm_gather_flags(0 flags_config)
+if(${flags} MATCHES "/MT")
+  set(gtest_force_shared_crt OFF)
+else()
+  set(gtest_force_shared_crt ON)
+endif()
+
+CGET_HAS_DEPENDENCY(googletest GITHUB google/googletest VERSION "${VERSION}" FINDNAME GTest OPTIONS -Dgtest_force_shared_crt=${gtest_force_shared_crt})
 set(ARGS_NO_FIND_PACKAGE "true")
