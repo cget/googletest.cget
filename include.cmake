a0fe324e19b50784d30c06900799619d38e6787f
JBerger <JBerger@melco.com>
2016-10-20 15:28:15 -0600

    updated version tag

--- include.cmake
+++ include.cmake
@@ -1,5 +1,5 @@
-if(NOT VERSION)
-       set(VERSION release-1.8.0)
+if(NOT CGET_REQUESTED_VERSION)
+       set(CGET_REQUESTED_VERSION release-1.8.0)
 endif()
 
 CGET_HAS_DEPENDENCY(ucm REGISTRY)
@@ -11,5 +11,5 @@ else()
   set(gtest_force_shared_crt ON)
 endif()
 
-CGET_HAS_DEPENDENCY(googletest GITHUB google/googletest VERSION "${VERSION}" FINDNAME GTest OPTIONS -Dgtest_force_shared_crt=${gtest_force_shared_crt})
+CGET_HAS_DEPENDENCY(googletest GITHUB google/googletest VERSION "${CGET_REQUESTED_VERSION}" FINDNAME GTest OPTIONS -Dgtest_force_shared_crt=${gtest_force_shared_crt})
 set(ARGS_NO_FIND_PACKAGE "true")
